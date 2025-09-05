8a6e24f08dd3dc09472c88bc58d384d42d030cf0
Pham Anh Khoa <miloemaster@gmail.com>
2025-09-05 15:49:09 +0700

    Initial Commit

---
+++ DapChuoi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DapChuoi
+//
+//  Created by Phạm Anh Khoa on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
